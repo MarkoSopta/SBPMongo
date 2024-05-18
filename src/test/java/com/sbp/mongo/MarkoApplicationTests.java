3a42105ad7f53fd89792ed2228d0891b2bf7950f
MarkoSopta <77636140+MarkoSopta@users.noreply.github.com>
2024-05-18 15:26:49 +0200

    MongoExpress

---
+++ src/test/java/com/sbp/mongo/MarkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbp.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
